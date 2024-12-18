25d606acc41fcae26988dce30b55cd5ccb46cba1
Guilherme Serra <guilhermeserrasd@gmail.com>
2024-12-18 00:02:01 -0300

    setando volume fixo para todos os áudios, reorganizando conteudo acima do quadro do jogo

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -2,3 +2,8 @@ export const audio = new Audio('../assets/audio/audio.mp3');
 export const audio2 = new Audio('../assets/audio/easyandmedium.mp3');
 export const audio3 = new Audio('../assets/audio/gameover.mp3');
 export const audio4 = new Audio('../assets/audio/hardmode.mp3');
+
+audio.volume = 0.3
+audio2.volume = 0.3
+audio3.volume = 0.3
+audio4.volume = 0.3
